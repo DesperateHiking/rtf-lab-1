342c1ac58938d63bd98acddecdf19ead07b21535
Desperate Hiking <perevechenkov1999@gmail.com>
2019-10-10 11:59:40 +0500

    не ну это смэрть

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function addNumber(array, number) {
-    if (array.every(item => Number.isInteger(item)))
+    if (array.every(item => Number.isInteger(item) && item.toString().length === 1))
         return (parseInt(array.join(''), 10) + number).toString().split('').map(Number);
     return null;
 }

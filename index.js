1d7d7ef63c444cc5343daa483b8b5184db81cc9d
Desperate Hiking <perevechenkov1999@gmail.com>
2019-10-13 17:14:44 +0500

    super misunderstandable

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 function addNumber(array, number) {
-    if (array.some(item => !Number.isInteger(item) || item.toString().length !== 1))
-        return null;
-    return (parseInt(array.join(''), 10) + number).toString().split('').map(Number);
+    return array.some(item => !Number.isInteger(item) || item.toString().length !== 1) ?
+        null : (parseInt(array.join(''), 10) + number).toString().split('').map(Number);
 }
-//
+
 module.exports = {
     addNumber
 };
